339bac14f96aa2ac6b53f16f6381da4b8f2d824f
piaopiao <643696403@qq.com>
2019-08-06 20:46:28 +0800

    另一个人第三次提交更新内容

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -8,5 +8,7 @@ public class Main {
         System.out.println("HelloWorld!");
         System.out.println("再次提交新的代码到服务器");
         System.out.println("另一个人提交新的代码到服务器");
+        System.out.println("另一个人三次提交更新代码到服务器");
+		
     }
 }
